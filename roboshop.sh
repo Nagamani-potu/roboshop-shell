8a0554ba47198ffaa372854366cdf66aef51cf4c
Nagamani-potu <nagamanikp32@gmail.com>
2025-05-22 21:53:47 +0530

    adding scripts

--- roboshop.sh
+++ roboshop.sh
@@ -2,18 +2,19 @@
 
 AMI_ID="ami-03265a0778a880afb"
 SG_ID="sg-087e7afb3a936fce7"
-INSTANCES=("mongodb", "redis", "mysql", "rabbitmq", "catalogue", "cart","user", "shipping", "payment", "web")
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "web")
 
-for -i in "{$INSTANCES[@]}"
+for i in "${INSTANCES[@]}"
 do
-   if [ $i == "mongodb" ] || [ $i == "shipping" ] || [ $i == "mysql" ]
-   then
-      INSTANCE_TYPE="t3.small"
-    else
-       INSTANCE_TYPE="t2.micro"
-    fi
+  echo instance is $i
+  if [ "$i" == "mongodb" ] || [ "$i" == "shipping" ] || [ "$i" == "mysql" ]; then
+    INSTANCE_TYPE="t3.small"
+  else
+    INSTANCE_TYPE="t2.micro"
+  fi
 
-      aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7
+  aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-087e7afb3a936fce7
 done
+
 
 
