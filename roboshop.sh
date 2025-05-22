3c84866b6ec7f9ce716029748d5324a917a7223a
Nagamani-potu <nagamanikp32@gmail.com>
2025-05-22 22:06:45 +0530

    adding scripts

--- roboshop.sh
+++ roboshop.sh
@@ -13,7 +13,6 @@ do
   fi
 
   IP_ADDRESS=(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-07891a5d518ef74be --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-  --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 done
 
