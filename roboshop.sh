b1abe50ae3796e72a58d48ea9fb715dbf4aa5fc1
Nagamani-potu <nagamanikp32@gmail.com>
2025-05-22 22:06:00 +0530

    adding scripts

--- roboshop.sh
+++ roboshop.sh
@@ -6,14 +6,15 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipp
 
 for i in "${INSTANCES[@]}"
 do
-  echo instance is $i
   if [ "$i" == "mongodb" ] || [ "$i" == "shipping" ] || [ "$i" == "mysql" ]; then
     INSTANCE_TYPE="t3.small"
   else
     INSTANCE_TYPE="t2.micro"
   fi
 
-  aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-07891a5d518ef74be --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+  IP_ADDRESS=(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-07891a5d518ef74be --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+  --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
 done
 
 
