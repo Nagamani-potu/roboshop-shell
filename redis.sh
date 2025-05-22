2069901b124deff77d019b4b734be0746986d806
Nagamani-potu <nagamanikp32@gmail.com>
2025-05-22 14:50:32 +0530

    adding scripts

--- redis.sh
+++ redis.sh
@@ -5,56 +5,52 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+exec &>$LOGFILE
 
-echo "scripting started execution at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
-Validate(){
-    if [ $? -ne 0 ]
-then 
-  echo -e " $2...$R failed $N"
-  exit 1
-else
-   echo -e " $2 ...$G success $N"
-fi
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
+
 if [ $ID -ne 0 ]
 then
-   echo -e " $R ERROR:: Please run with root access $N"
-   exit 1
-else 
-  echo -e " $G you are a root user"
-fi
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.5.rpm -y &>> $LOGFILE
 
-VALIDATE $? "Installing remi relase file"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
-dnf module reset redis -y &>> $LOGFILE
+VALIDATE $? "Installing Remi release"
 
-VALIDATE $? "Resetting redis module"
-
-dnf module enable redis:remi-6.2 -y &>> $LOGFILE
-
-VALIDATE $? "Enabling redis module"
-
-dnf install redis -y &>> $LOGFILE
-
-VALIDATE $? "Installing redis"
-
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>> $LOGFILE
-
-VALIDATE $? "adding remote access"
-
-systemctl enable redis &>> $LOGFILE
+dnf module enable redis:remi-6.2 -y
 
 VALIDATE $? "enabling redis"
 
-systemctl start redis &>> $LOGFILE
+dnf install redis -y
 
-VALIDATE $? "Starting redis"
+VALIDATE $? "Installing Redis"
 
-systemctl status redis &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 
-VALIDATE $? "To check status of redis"
+VALIDATE $? "allowing remote connections"
+
+systemctl enable redis
+
+VALIDATE $? "Enabled Redis"
+
+systemctl start redis
+
+VALIDATE $? "Started Redis"
