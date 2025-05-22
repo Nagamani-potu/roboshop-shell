a05252c4cbbb8939564c3ba661e4cbc7530232d7
Nagamani-potu <nagamanikp32@gmail.com>
2025-05-22 21:59:35 +0530

    adding scripts

--- roboshop.sh
+++ roboshop.sh
@@ -13,7 +13,7 @@ do
     INSTANCE_TYPE="t2.micro"
   fi
 
-  aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-07891a5d518ef74be
+  aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-07891a5d518ef74be --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
 done
 
 
